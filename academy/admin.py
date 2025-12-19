bb4353403a412ffb2e5b799ebe9ee65303a5e37d
Vignesh21J <vignesh11212003@gmail.com>
2025-12-19 17:11:24 +0530

    feat:added models and customized the admin panel

--- academy/admin.py
+++ academy/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Course, Trainer, Student
 
 # Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'duration']
+    search_fields = ['name', 'description']
+    ordering = ["id"]
+
+admin.site.register(Course, CourseAdmin)
+
+
+class TrainerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'full_name','expertise']
+    list_filter = ['expertise']
+    search_fields = ['first_name', 'last_name', 'email']
+    list_display_links = ['full_name']
+    ordering = ["id"]
+admin.site.register(Trainer, TrainerAdmin)
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'full_name','email', 'trainer', 'enrolled_course', 'is_active']
+    list_filter = ['trainer', 'enrolled_course']
+    search_fields = ['first_name', 'last_name', 'email']
+    list_editable = ['is_active']
+    list_display_links = ['full_name', 'email']
+    ordering = ["id"]
+admin.site.register(Student, StudentAdmin)
--- academy/models.py
+++ academy/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    duration = models.IntegerField(verbose_name="Duration (in weeks)")
+    # image = models.ImageField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Trainer(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    expertise = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True)
+    # profile_image = models.ImageField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    enrolled_course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True)
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
